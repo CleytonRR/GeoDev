508c335db587e571de705b588ffd6abea1fc2bfe
cleytonRR <cleytonrodrigues69@live.com>
2020-01-15 17:30:31 -0300

    installing cors and settings for this

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 mongoose.connect('mongodb+srv://free:js2014@cluster0-a9u1k.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
