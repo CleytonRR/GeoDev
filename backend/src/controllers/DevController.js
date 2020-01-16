ced352ca01d7226f09f77bf33ed668f3947f3bdc
cleytonRR <cleytonrodrigues69@live.com>
2020-01-16 11:20:00 -0300

    Added try catch

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -10,29 +10,34 @@ module.exports = {
 
     async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body
+        try {
+            var dev = await Dev.findOne({ github_username })
 
-        var dev = await Dev.findOne({ github_username })
+            if (!dev) {
+                const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
 
-        if (!dev) {
-            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+                const { name = login, avatar_url, bio } = apiResponse.data
+                const techsArray = parseStringAsArray(techs)
 
-            const { name = login, avatar_url, bio } = apiResponse.data
-            const techsArray = parseStringAsArray(techs)
+                const location = {
+                    type: 'Point',
+                    coordinates: [longitude, latitude]
+                }
 
-            const location = {
-                type: 'Point',
-                coordinates: [longitude, latitude]
+                dev = await Dev.create({
+                    github_username,
+                    name,
+                    avatar_url,
+                    bio,
+                    techs: techsArray,
+                    location,
+                })
             }
+            return response.json(dev)
 
-            dev = await Dev.create({
-                github_username,
-                name,
-                avatar_url,
-                bio,
-                techs: techsArray,
-                location,
-            })
+        } catch (error) {
+            console.log(error)
+            return response.json({ message: 'Erro ao salvar' })
         }
-        return response.json(dev)
     }
 }
