a7951fff2f9b1616b7d7d88f6e263f05b1b86aa8
cleytonRR <cleytonrodrigues69@live.com>
2020-01-16 14:10:01 -0300

    Created page of maps with style

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,22 +1,82 @@
-import React, { useEffect } from 'react'
-import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Image, View, Text } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
-function Main() {
+function Main({ navigation }) {
+    const [currentRegion, setCurrentRegion] = useState(null)
+
     useEffect(() => {
         async function loadInitialPosition(){
+            const { granted } = await requestPermissionsAsync()
+            if(granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                })
 
+                const { latitude, longitude } = coords
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                })
+            }
         }
         loadInitialPosition()
     }, [])
 
-    return <MapView style={styles.map} />
+    if(!currentRegion) {
+        return null
+    }
+
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{ latitude: -3.4626136, longitude: -41.5557458}}>
+                <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/1661112?s=400&v=4'}} />
+                <Callout onPress={() => {
+                    navigation.navigate('Profile', { github_username: 'mazulo' })
+                }}>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Patrick Mazulo</Text>
+                        <Text style={styles.devBio}>Software Engineer at Carta. In my spare time I really like to play with Python, games and watch animes/TV shows :) </Text>
+                        <Text style={styles.devTechs}>Python, Django, Pyramid</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
+    )
+        
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1
+    }, 
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF'
+    },
+
+    callout: {
+        width: 260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+
+    devTechs: {
+        marginTop: 5,
     }
 })
 
