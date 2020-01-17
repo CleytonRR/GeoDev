f9d611510e6d030311a76ef5e24163f60355a437
cleytonRR <cleytonrodrigues69@live.com>
2020-01-17 15:13:39 -0300

    Created websocket.js and starting initial settings for connection from socket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const routes = require('./routes')
 const cors = require('cors')
+const http = require('http')
+const routes = require('./routes')
+const { setupWebSocket } = require('./websocket')
 
 const app = express()
+const server = http.Server(app)
+setupWebSocket(server)
+
 mongoose.connect('mongodb+srv://free:js2014@cluster0-a9u1k.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -12,6 +17,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => {
+server.listen(3333, () => {
     console.log('Executando o servdor na porta 3333')
 })
